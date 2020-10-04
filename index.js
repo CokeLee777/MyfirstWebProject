0e72da8d6bebcc0e26fdb13aeaeafc57c55e5acd
Lee12312 <akak640@kyonggi.ac.kr>
2020-10-04 17:06:57 +0900

    exercise_1004

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// alert('Im working. Im JS. Im beautiful. Im worth it')
-const what = "hi";
-let b = 5;
-console.log(b,what);
+const title = document.getElementById('title');
+
+console.log(title);
